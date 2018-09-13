e6616876fca3875b4e833b0a7d0f4aa734f0eccd
Felix Heras <heras.felix@gmail.com>
2018-09-13 16:39:41 +0200

    Feat: New query

--- src/queries.js
+++ src/queries.js
@@ -6,3 +6,13 @@ export const ARTICLES_QUERY = `{
     title
   }
 }`;
+
+export const ARTICLE_QUERY = (id) => `{
+  article(id: "${id}") {
+    author
+    content
+    published
+    tags
+    title
+  }
+}`;
